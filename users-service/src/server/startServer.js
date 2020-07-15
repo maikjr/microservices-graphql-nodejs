25a003719ec79ca788649a93d10a448b8d6a73f4
agvivadigital <agenciavivadigital@gmail.com>
2020-07-15 20:51:50 -0300

    first commit

---
+++ users-service/src/server/startServer.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import cors from 'cors';
+import accessEnv from '@root/helpers/accessEnv';
+
+const PORT = accessEnv('PORT', 7101);
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(
+    cors({
+      origin: (origin, cb) => cb(null, true),
+      credentials: true,
+    }),
+);
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.info(`Users service listening on ${PORT}`);
+});
